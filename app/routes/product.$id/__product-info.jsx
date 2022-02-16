3009211febdf5ba2552886a90591e80bfc2fcdb9
Jacob Ebey <jacob.ebey@live.com>
2022-02-16 23:51:42 +0700

    added another loader

--- app/routes/product.$id/__product-info.jsx
+++ app/routes/product.$id/__product-info.jsx
@@ -1,10 +1,16 @@
-import { Outlet } from "remix";
+import { Outlet, useLoaderData } from "remix";
+
+export function loader({ params: { id } }) {
+  return `Info for product ${id}`;
+}
 
 export default function ProductInfo() {
+  let info = useLoaderData();
+
   return (
     <div style={{ display: "flex" }}>
       <div style={{ border: "1px solid red" }}>
-        <h2>Info...</h2>
+        <h2>{info}</h2>
       </div>
       <Outlet />
     </div>
